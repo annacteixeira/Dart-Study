fd741cfd34f15c3c74d8340128e55c2fd3a84031
Anna Chandra <annaftteixeira@gmail.com>
2025-06-10 16:16:21 -0300

    data types in dart

---
+++ dart-basics/data-types.dart
@@ -0,0 +1,27 @@
+void main() {
+
+  // Strings
+  String firstName = "Anna";
+  print("String: $firstName");
+
+  // Integers
+  int intNum = 33;
+  print("Integer: $intNum");
+
+  // Doubles
+  double doubleNum = 33.33;
+  print("Double: $doubleNum");
+
+  // Booleans
+  bool myFalseBool = false;
+  bool myTrueBool = true;
+
+  print("False Boolean: $myFalseBool");
+  print("True Boolean: $myTrueBool");
+
+  // Dynamic
+  dynamic fullName = "Anna Chandra";
+  print("Dynamic: $fullName");
+
+  // Other Data Types are Lists and Maps
+}
