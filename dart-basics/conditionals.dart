ac635fbcc23fa3c250cce24764a5c250b8e1643f
Anna Chandra <annaftteixeira@gmail.com>
2025-06-10 16:52:58 -0300

    conditionals in dart

---
+++ dart-basics/conditionals.dart
@@ -0,0 +1,11 @@
+void main() {
+  var num = 5;
+
+  if(num == 5){
+    print("The number is 5!");
+  } else if(num == 3) {
+    print("The number is 3!");
+  } else {
+    print("The number is not 5 or 3, it is $num!");
+  }
+}
